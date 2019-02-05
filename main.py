4f6e6efbe965449bba5f8b808db73ef5684b1ca9
Rachit Agarwal <rachitagarwal0202@gmail.com>
2019-02-05 21:47:50 +0100

    initial pieces, user color selection

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 
 import numpy as np
 
-
 # Creates empty game board
 def createEmptyBoard():
 	board = []
@@ -11,16 +10,50 @@ def createEmptyBoard():
 	for row in range(0,size):
 		board.append([])
 		for col in range(0,size):
-			# 0 = empty, 1 = white, 2 = black
-			board[row].append(0)
+			# * = empty, O = white, X = black
+			# O is letter O not 0
+			board[row].append("*")
 	return board
+
+# Sets up initial game board
+def setupBoard():
+	board = createEmptyBoard()
+
+	board[3][3] = "O"
+	board[4][4] = "O"
+	board[3][4] = "X"
+	board[4][3] = "X"
+
+	return board
+
 
 # Prints the game board
 def printBoard(board):
+	print("Current Board:")
 	print np.matrix(board)
 
+# Handles user color selection
+def selectColor():
+	userColorInput = input("What color would you like to play as? Please enter 'black' or 'white'.")
+	if userColorInput = "black":
+		userColor = "black"
+		cpuColor = "white"
+	else:
+		userColor = "white"
+		cpuColor = "black"
+
+	print("Great! You are playing as %s. The CPU is playing as %s.", userColor, cpuColor)
+
+	return cpuColor
+
 def main():
-	board = createEmptyBoard()
+	board = setupBoard()
+
+	print("Welcome to Reversi! Let's get started.")
+
+	cpuColor = selectColor()
+
 	printBoard(board)
+
 
 main()
